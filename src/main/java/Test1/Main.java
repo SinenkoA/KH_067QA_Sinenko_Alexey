5e0133f7bc78a719b8bfc601e5682a86b9b31f49
SinenkoA <sinenko.alexey66@gmail.com>
2022-06-07 17:37:38 +0300

    Finish Task1

--- src/main/java/Test1/Main.java
+++ src/main/java/Test1/Main.java
@@ -18,10 +18,13 @@ public class Main {
         element.click();
         input = driver.findElement(By.xpath("//input[@name='search']"));
         input.sendKeys("iphone");
-        input.submit();
+        element = driver.findElement(By.xpath("//button [@class='button button_color_green button_size_" +
+                "medium search-form__submit ng-star-inserted']"));
+        element.click();
         element = driver.findElement(By.xpath("//ul[@class='catalog-grid ng-star-inserted']/li[1]"));
         element.click();
-        element = driver.findElement(By.xpath("//app-buy-button[@goods_id='260862296']"));
+        element = driver.findElement(By.xpath("//button [@class='buy-button button button--with-icon" +
+                " button--green button--medium ng-star-inserted']"));
         element.click();
         driver.quit();
     }
