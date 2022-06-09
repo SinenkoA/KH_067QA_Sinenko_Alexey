f48e7f74c13b8d03cf300d275f9107a091ae8d10
SinenkoA <sinenko.alexey66@gmail.com>
2022-06-09 16:37:15 +0300

    Finish Task1

--- src/main/java/Test1/Main.java
+++ src/main/java/Test1/Main.java
@@ -11,25 +11,31 @@ public class Main {
     public static void main(String[] args) {
 //        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //open Google
         driver.get("https://www.google.com/");
         driver.manage().window().maximize();
+        //select the search field
         WebElement input = driver.findElement(By.xpath(" //input[@name='q']"));
+        //enter the data
         input.sendKeys("rozetka ua");
         input.submit();
-        WebElement element = driver.findElement(By.xpath(" //h3[@class='LC20lb MBeuO DKV0Md']"));
-        element.click();
-        input = driver.findElement(By.xpath("//input[@name='search']"));
-        input.sendKeys("iphone");
-        element = driver.findElement(By.xpath("//button [@class='button button_color_green button_size_" +
-                "medium search-form__submit ng-star-inserted']"));
-        element.click();
-        element = driver.findElement(By.xpath("//ul[@class='catalog-grid ng-star-inserted']/li[1]"));
-        element.click();
-        element = driver.findElement(By.xpath("//button [@class='simple-slider__control simple-slider__control--next']"));
-        element.click();
-        element = driver.findElement(By.xpath("//button [@class='buy-button button button--with-icon button--green button--medium ng-star-inserted']"));
-        element.click();
+        //go to the first option as a result of the search
+        driver.findElement(By.xpath(" //h3[@class='LC20lb MBeuO DKV0Md']")).click();
+        //select the search field
+        driver.findElement(By.xpath("//input[@name='search']")).sendKeys("iphone");
+        //confirm the request by clicking on the search button
+        driver.findElement(By.xpath("//button [@class='button button_color_green button_size_" +
+                "medium search-form__submit ng-star-inserted']")).click();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        //go to the first option as a result of the search
+        driver.findElement(By.xpath("//ul[@class='catalog-grid ng-star-inserted']/li[1]")).click();
+        //Press the change photo button
+        driver.findElement(By.xpath("//button [@class='simple-slider__control " +
+                "simple-slider__control--next']")).click();
+        //click on the buy button
+        driver.findElement(By.xpath("//button [@class='buy-button button button--with-icon " +
+                "button--green button--medium ng-star-inserted']")).click();
+        //close the browser
         driver.quit();
     }
 
